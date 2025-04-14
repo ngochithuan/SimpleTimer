ba6912a7ad23c90800a0dec97826be7a50447923
NgoThuan <ngochithuan.dev@gmail.com>
2025-04-14 22:45:08 +0700

    add input time, beep sound

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,34 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.awt.Toolkit;
+
 
 public class Main {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime sysTime = LocalTime.now();
         String currentTime = sysTime.format(formatter);
-        LocalTime targetTime = LocalTime.parse("22:05");
-        while (true){
-            if (currentTime.equals(targetTime.toString())) {
-                System.out.println("Beep beep");
-                break;
+
+
+        String inputTime = "";
+        Scanner scanner = new Scanner(System.in);
+        while (!inputTime.equals("stop")){
+            System.out.println("Pick a time");
+            inputTime = scanner.next();
+            LocalTime targetTime = LocalTime.parse(inputTime);
+            while (true){
+                if (currentTime.equals(targetTime.toString())) {
+                    System.out.println("Beep beep");
+                    Toolkit.getDefaultToolkit().beep();
+                    break;
+                }
+                sysTime = LocalTime.now();
+                currentTime = sysTime.format(formatter);
             }
-            sysTime = LocalTime.now();
-            currentTime = sysTime.format(formatter);
         }
+
+        scanner.close();
         return;
     }
 }
